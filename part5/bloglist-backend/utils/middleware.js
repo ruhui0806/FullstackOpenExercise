f35bf79c0c1a44e39e8531f812d62f9ad4b3787b
文静 <kulta@bunseis-MacBook-Air.local>
2022-11-10 14:13:37 +0200

    fix the show and hide for single item bug

--- part5/bloglist-backend/utils/middleware.js
+++ part5/bloglist-backend/utils/middleware.js
@@ -1,8 +1,7 @@
-
 // const User = require("../models/User")
 const logger = require('./logger')
 const jwt = require('jsonwebtoken')
-const User = require("../models/User")
+const User = require('../models/User')
 
 const requestLogger = (request, response, next) => {
     logger.info('Method:', request.method)
@@ -14,17 +13,16 @@ const requestLogger = (request, response, next) => {
 
 const errorHandler = (error, request, response, next) => {
     logger.error(error.message)
-    if (error.name === "CastError") {
-        return response.status(400).send({ error: 'malformatted id' })
-    }
-    else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
-    else if (error.name === 'JsonWebTokenError') {
-        return response.status(401).json({ error: 'missing token or invalid token' })
-    }
-    else if (error.name === 'TokenExpiredError') {
-        return response.status(401).json({ error: 'token expired' })
+    if (error.name === 'CastError') {
+        return response.status(400).send({error: 'malformatted id'})
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({error: error.message})
+    } else if (error.name === 'JsonWebTokenError') {
+        return response
+            .status(401)
+            .json({error: 'missing token or invalid token'})
+    } else if (error.name === 'TokenExpiredError') {
+        return response.status(401).json({error: 'token expired'})
     }
     // logger.error(error.message)
     next(error)
@@ -52,46 +50,14 @@ const userExtractor = async (request, response, next) => {
     return next()
 }
 
-////combine token extractor with user Extractor:
-// const userExtractor = async (request, response, next) => {
-//     const authorization = request.get('authorization')
-//     if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-//         token = authorization.substring(7)
-//     }
-//     const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET)
-//     if (!decodedToken.id) {
-//         return response.status(401).json({ error: 'token missing or invalid' })
-//     }
-//     const user = await User.findById(decodedToken.id)
-//     request.user = user
-//     next()
-//     return
-// }
-// const token = request.token
-// const decodedToken = jwt.verify(token, process.env.SECRET)
-// if (!token || !decodedToken.id) {
-//     return response.status(401).json({ error: 'token missing or invalid' })
-// }
-
-// else if (body.title === undefined || body.url === undefined) {
-//     return response.status(400).json({ error: 'content missing' })
-// }
-
-// const user = await User.findById(decodedToken.id)
-
-
-
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+    response.status(404).send({error: 'unknown endpoint'})
 }
-
 
 module.exports = {
     requestLogger,
     unknownEndpoint,
     errorHandler,
     tokenExtractor,
-    userExtractor
+    userExtractor,
 }
-
-
