3ea8d0e814f33f03c8b6ebcc6a7704d635f7826f
文静 <ruhuiwen0608@gmail.com>
2023-01-08 17:27:10 +0200

    fix the .env file expose problem by deleting it from github

--- part7/Bloglist/bloglist-backend/models/Blog copy.js
+++ part7/Bloglist/bloglist-backend/models/Blog copy.js
@@ -1,6 +1,33 @@
 const mongoose = require('mongoose')
 
 const blogSchema = new mongoose.Schema({
+<<<<<<< HEAD
+  title: { type: String, required: true },
+  author: String,
+  url: { type: String, required: true },
+  likes: Number,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }
+})
+
+blogSchema.pre('remove', function (next) {
+
+  this.model('User').remove({ blogs: this._id }, next);
+})
+
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedOBJ) => {
+    returnedOBJ.id = returnedOBJ._id.toString()
+    delete returnedOBJ._id
+    delete returnedOBJ.__v
+  }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
+=======
     title: { type: String, required: true },
     author: String,
     url: { type: String, required: true },
@@ -30,31 +57,4 @@ blogSchema.set('toJSON', {
 })
 
 module.exports = mongoose.model('Blog', blogSchema)
-
-// const blogSchema = new mongoose.Schema({
-// <<<<<<< HEAD
-//   title: { type: String, required: true },
-//   author: String,
-//   url: { type: String, required: true },
-//   likes: Number,
-//   user: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: "User"
-//   }
-// })
-
-// blogSchema.pre('remove', function (next) {
-
-//   this.model('User').remove({ blogs: this._id }, next);
-// })
-
-// blogSchema.set('toJSON', {
-//   transform: (document, returnedOBJ) => {
-//     returnedOBJ.id = returnedOBJ._id.toString()
-//     delete returnedOBJ._id
-//     delete returnedOBJ.__v
-//   }
-// })
-
-// module.exports = mongoose.model('Blog', blogSchema)
-// =======
+>>>>>>> part7-redux-7.21
