83b2249a8c91bfc00aa7e1d25254eb4e1e37e5ed
文静 <ruhuiwen0608@gmail.com>
2023-05-31 11:34:21 +0300

    finish exercie 13.4

--- part13/index.js
+++ part13/index.js
@@ -6,16 +6,70 @@ const app = express();
 app.use(express.json());
 
 const sequelize = new Sequelize(process.env.DATABASE_URL);
-const main = async () => {
-  try {
-    await sequelize.authenticate();
-    const blogs = await sequelize.query("SELECT * FROM blogs", {
-      type: QueryTypes.SELECT,
-    });
-    console.log(blogs);
-    sequelize.close();
-  } catch (err) {
-    console.error("Unable to connect to the database:", err);
+
+class Blog extends Model {}
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+      type: DataTypes.TEXT,
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: "blog",
   }
-};
-main();
+);
+Blog.sync();
+app.get("/api/blogs", async (req, res) => {
+  try {
+    const blogs = await Blog.findAll();
+    console.log(JSON.stringify(blogs));
+    res.json(blogs);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+app.post("/api/blogs", async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body);
+    res.json(blog);
+  } catch {
+    return res.status(400).json({ error });
+  }
+});
+
+app.delete("/api/blogs/:id", async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id);
+  if (blog) {
+    await Blog.destroy({
+      where: { id: blog.id },
+    });
+    res.status(204).end();
+  } else {
+    return res.status(404).json({ error });
+  }
+});
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- part13/readme_part13.md
+++ part13/readme_part13.md
@@ -1,4 +1,6 @@
 28.05.2023: working on part13_a
 
 Difference between text and string data types in Sequelize:
+
+The postgres fly.app name is: fs-blog-app
 A string field has a limit of 255 characters, whereas a text field has a character limit of 30,000 characters
