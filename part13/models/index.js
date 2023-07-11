aa6073984a6f9c11b080bfb1d3fa13ebec2151d4
文静 <ruhuiwen0608@gmail.com>
2023-07-11 16:22:05 +0300

    add through table for blogs and users (many-to-many)

--- part13/models/index.js
+++ part13/models/index.js
@@ -1,8 +1,13 @@
 const User = require("./user");
 const Blog = require("./blog");
+const ReadingLists = require("./reading_lists");
 
-User.hasMany(Blog);
 Blog.belongsTo(User);
+User.hasMany(Blog);
+
+Blog.belongsToMany(User, { through: ReadingLists, as: "users_marked" });
+User.belongsToMany(Blog, { through: ReadingLists, as: "readings" });
+
 //instead of using sync to update changes in database, we use migration to manage changes in source code to databases
 // Blog.sync({ alter: true });
 // User.sync({ alter: true });
@@ -10,4 +15,5 @@ Blog.belongsTo(User);
 module.exports = {
   Blog,
   User,
+  ReadingLists,
 };
---
+++ part13/models/reading_lists.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require("sequelize");
+
+const { sequelize } = require("../utils/db");
+
+class ReadingLists extends Model {}
+
+ReadingLists.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: "users", key: "id" },
+    },
+    blogId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: "blogs", key: "id" },
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: "readingLists",
+  }
+);
+module.exports = ReadingLists;
