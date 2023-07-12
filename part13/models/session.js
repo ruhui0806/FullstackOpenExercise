dcbfafeb26a33c2f520bfdfbee815ebed42e54e5
文静 <ruhuiwen0608@gmail.com>
2023-07-12 14:59:33 +0300

    update models/session.js: pass sequelize instance to model

--- part13/models/session.js
+++ part13/models/session.js
@@ -3,12 +3,20 @@ const { sequelize } = require("../utils/db");
 
 class Session extends Model {}
 
-Session.init({
-  token: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    primaryKey: true,
+Session.init(
+  {
+    token: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+    },
   },
-});
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: "session",
+  }
+);
 
 module.exports = Session;
